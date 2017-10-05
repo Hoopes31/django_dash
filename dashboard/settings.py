976f033fba054c82d896fad5e259ea4925817ffb
Anthony <Anthony@Anthonys-iMac.local>
2017-10-04 23:48:12 -0700

    restful service

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -40,7 +40,15 @@ INSTALLED_APPS = [
     'apps.user',
     'apps.dressing',
     'apps.admin_dash',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser',
+    ],
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
